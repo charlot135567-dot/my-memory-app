e47e5d2cc3fa6ce4d626ed43128d53b496a82ebb
charlot135567-dot <charlot135567@gmail.com>
2026-03-15 17:34:53 +0800

    app.py

--- app.py
+++ app.py
@@ -2384,7 +2384,7 @@ its part of speech and meaning in this sentence must be clearly identified...等
                                 "saved_sheets": st.session_state.saved_entries,
                                 "type": type_select,
                                 "mode": st.session_state.content_mode,
-                                "date_added": datetime.now().strftime("%Y-%m-%d %H:%M")
+                                "date_added": datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
                             }
                             st.session_state.sentences[ref] = full_data
                             save_sentences(st.session_state.sentences)
